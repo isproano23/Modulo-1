581145c02436d0de8c330a5dbe7d50e09c187cc2
isproano23 <sebasproano917@gmail.com>
2025-11-14 08:27:19 -0500

    EJERCICIO GIT

---
+++ ejerciciogit/ejercicio.js
@@ -0,0 +1,6 @@
+saludar = function () {
+  let cmpimagensaludo;
+  cmpimagensaludo = document.getElementById("imgsaludo");
+  cmpimagensaludo.src = "./imagenes/homer.gif";
+  console.log("Ingresa a Saludar");
+};
